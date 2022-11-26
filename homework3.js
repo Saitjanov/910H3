ee3515fff4c31af933518a012074349967f8152f
Saitjanov <saitjanovtimur@gmail.com>
2022-11-26 09:35:57 +0500

    first commit

---
+++ homework3.js
@@ -0,0 +1,42 @@
+// let name = 'alex'
+// let money = 10000
+// let accaunt = 7777
+
+// let userName = prompt('Как вас зовут?').toLowerCase().trim()
+
+// if(userName === name.toLowerCase()); else{alert('Пользователь не найден')} {
+//     let userAcc = +prompt('Укажите ваш пороль')
+
+//     if(userAcc === accaunt) {
+//         let cashOut = +prompt('Сколько хотите снять?')
+
+//         if(cashOut <= money) {
+//             alert(`
+//             У вас было: ${money}
+//             У вас осталось: ${money - cashOut}
+//             `)
+//         } else{
+//             alert('У вас нет столько денег на счету')
+//         }
+//     }
+// }
+
+
+
+// let name2 = prompt('Whats your name').toLowerCase().trim()
+
+// if(name2[0] === 'a') {
+//     let age = prompt('your age') 
+// if(age >= 20 && age <= 40) {
+//     let money = prompt('how much you have money')
+// if (money >= 100) { 
+//     alert(`welcome to club ${name2} `)
+// } else {
+//     alert(`sorry ${name2}`)
+// }
+// } else {
+//     alert(`sorry ${name2}`)
+// }
+// } else {
+//     alert(`sorry ${name2}`)
+// }
